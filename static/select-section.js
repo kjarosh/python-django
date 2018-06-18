23543edcc6741c07c600003079973642b3e25005
Kamil Jarosz <kamil09875@gmail.com>
2018-06-18 19:26:41 +0200

    Fix links

--- static/select-section.js
+++ static/select-section.js
@@ -3,9 +3,12 @@ var elements = document.getElementsByClassName("select-section");
 for(var i = 0; i < elements.length; i++){
 	var el = elements[i];
 	el.onchange = function(){
-		if(el.value == '') return;
-		
-		url = el.dataset.urlformat.replace(el.dataset.urlreplace, el.value);
-		window.location.href = url;
+		if(el.value == ''){
+			// global
+			window.location.href = el.dataset.globalurlformat;
+		}else{
+			url = el.dataset.urlformat.replace(el.dataset.urlreplace, el.value);
+			window.location.href = url;
+		}
 	}
 }
